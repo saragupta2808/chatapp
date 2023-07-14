9e289a5ce1eb8f221b042e8e40a4582625a3c2ad
Sara Gupta <saragupta1924@gmail.com>
2023-07-14 21:48:14 +0530

    na hoga

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use('/api/message',messageRoutes)
 const __dirname1 = path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1,'..','/frontend/chat-app/dist')));
+    app.use(express.static(path.join(__dirname1,'/frontend/chat-app/dist')));
     app.get('*', (req,res)=>{
         res.sendFile(path.join(__dirname1, "/frontend/chat-app/dist/index.html"))
     });
