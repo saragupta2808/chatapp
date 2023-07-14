337f0ed95ff6bf81f11bb8f1488402248db99ca3
Sara Gupta <saragupta1924@gmail.com>
2023-07-14 22:08:06 +0530

    hogaya

--- frontend/chat-app/src/axios.js
+++ frontend/chat-app/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // we need to pass the baseURL as an object
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://talkative-6pbd.onrender.com",
 });
 
 export default API;
